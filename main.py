2e642327d275b2f87b86196f56fe671129083b6c
GabrielMontAraujo <graubx25@gmail.com>
2024-03-13 22:13:36 -0300

    inclusion of new attributes

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-robots = {
-    1: {"name": "HBA", "type": "happy"},
-    2:{"name": "HBO", "type": "obedient"},
-    3:{"name": "HBX", "type": "drunk"},
+droids = {
+    1: {"model": "HBA", "type": "happy", "job": "analist", "status": "active"},
+    2:{"model": "HBO", "type": "obedient", "job": "guard", "status": "active"},
+    3:{"model": "HBX", "type": "drunk", "job": "barman", "status": "active"},
 }
 
 @app.get("/")
 def home():
-    return robots 
+    return droids 
 
-@app.get("/robots/{id_robot}")
-def list_robot(id_robot: int):
-    if id_robot in robots:
-        return robots[id_robot]
+@app.get("droids/{id_droid}")
+def list_droid(id_droid: int):
+    if id_droid in droids:
+        return droids[id_droid]
     else:
-        return "this robot doesn't exist"
+        return "this droid doesn't exist"
     
